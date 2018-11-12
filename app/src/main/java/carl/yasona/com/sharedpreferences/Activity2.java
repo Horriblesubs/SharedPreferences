3b9d87e8d39b4d81f308355b2f1f4e01472ee33a
HorribleSubs <2015081199@ust-ics.mygbiz.com>
2018-11-12 16:07:18 +0800

    Internal and External

--- app/src/main/java/carl/yasona/com/sharedpreferences/Activity2.java
+++ app/src/main/java/carl/yasona/com/sharedpreferences/Activity2.java
@@ -8,24 +8,44 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Activity2 extends AppCompatActivity {
     TextView tMsg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
         tMsg = findViewById(R.id.tvMsg);
     }
-    public void prevActivity(View v){
+
+    public void prevActivity(View v) {
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
     }
 
-    public void greet(View v){
+    public void greet(View v) {
         SharedPreferences sp = getSharedPreferences("data", MODE_PRIVATE);
         String name = sp.getString("uname", null); // s = key, s1 = defvalue
         String password = sp.getString("pass", null);
         String message = "Hello " + name + ". Your password is " + password;
         tMsg.setText(message);
+    }
+
+    public void greetFromInternal(View v) {
+        try {
+            FileInputStream fin = openFileInput("internal.txt");
+            int c;
+            StringBuffer buffer = new StringBuffer();
+            while ((c = fin.read()) != -1) {
+                buffer.append((char) c);
+            }
+            tMsg.setText("Good Afternoon, " + buffer);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/carl/yasona/com/sharedpreferences/MainActivity.java
+++ app/src/main/java/carl/yasona/com/sharedpreferences/MainActivity.java
@@ -1,15 +1,23 @@
 package carl.yasona.com.sharedpreferences;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
     TextView tName, tPwd;
+    String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,19 +27,77 @@ public class MainActivity extends AppCompatActivity {
         tPwd = findViewById(R.id.etPwd);
     }
 
-    public void nextActivity(View v){
+    public void nextActivity(View v) {
         Intent i = new Intent(this, Activity2.class);
         startActivity(i);
     }
 
-    public void saveData(View v){
-        String name = tName.getText().toString();
+    public void saveData(View v) {
+        name = tName.getText().toString();
         String pwd = tPwd.getText().toString();
         SharedPreferences sp = getSharedPreferences("data", MODE_PRIVATE); //paper
         SharedPreferences.Editor writer = sp.edit(); //pen
         writer.putString("uname", name);
         writer.putString("pass", pwd);
         writer.commit();
-        Toast.makeText(this, "Saved Successfully",Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Saved Successfully", Toast.LENGTH_LONG).show();
+    }
+
+    public void saveInternal(View v) {
+        FileOutputStream fos = null;
+        String name2 = tName.getText().toString();
+        try {
+            fos = openFileOutput("internal.txt", MODE_PRIVATE);
+            fos.write(name2.getBytes());
+            Toast.makeText(this, "Data saved in the internal file...", Toast.LENGTH_LONG);
+        } catch (Exception e) {
+            Toast.makeText(this, "Error writing to internal file..", Toast.LENGTH_LONG);
+        }finally{
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void saveExternal(View v){
+        File folder = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+        File file = new File(folder, "external.txt");
+        FileOutputStream fos = null;
+        String name2 = tName.getText().toString();
+        try {
+            fos = new FileOutputStream(file);
+            fos.write(name2.getBytes());
+            Toast.makeText(this, "Data saved in the external file...", Toast.LENGTH_LONG);
+        } catch (Exception e) {
+            Toast.makeText(this, "Error writing to external file..", Toast.LENGTH_LONG);
+        }finally{
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void saveCache(View v){
+        File folder = getCacheDir(); //External Cache - getExternalCacheDir();
+        File file = new File(folder, "cache.txt");
+        FileOutputStream fos = null;
+        String name2 = tName.getText().toString();
+        try {
+            fos = new FileOutputStream(file);
+            fos.write(name2.getBytes());
+            Toast.makeText(this, "Data saved in the external file...", Toast.LENGTH_LONG);
+        } catch (Exception e) {
+            Toast.makeText(this, "Error writing to external file..", Toast.LENGTH_LONG);
+        }finally{
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
+
